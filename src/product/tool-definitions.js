1075addbbe7bbe3f383c1f204962b296331c7785
abdelaziz-maysara-hm <61553486+abdelaziz-maysara-hm@users.noreply.github.com>
2026-08-10 14:23:01 +0300

    Restore tool-definitions.js (was accidentally overwritten) + register currency-converter

--- src/product/tool-definitions.js
+++ src/product/tool-definitions.js
@@ -1,1 +1,4 @@
-PLACEHOLDER
+import { retiredToolIds } from './retired-tool-ids.js';
+import { converterDefinitions } from './definitions/converters.js';
+import { currencyConverterToolDefinitions } from './definitions/currency-converter-tool.js';
+import { dateTimeDefinitions } from './definitions/date-time.js';
